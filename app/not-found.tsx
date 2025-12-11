02fde71b602629cd000533057e386d63f177ea5a
Bogdan-uk <656olololol@gmail.com>
2025-12-11 11:57:14 +0200

    ex1 -- fonts, meta, open-gr.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,24 @@
 import css from './not-found.module.css';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Сторінку не знайдено — 404',
+  description:
+    'Ця сторінка не існує. Можливо, ви перейшли за неправильним посиланням або сторінку було видалено.',
+  openGraph: {
+    title: 'Сторінку не знайдено — 404',
+    description:
+      'Ця сторінка не існує. Можливо, ви перейшли за неправильним посиланням або сторінку було видалено.',
+    url: '/not-found',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub 404 Open Graph Image',
+      },
+    ],
+  },
+};
 const NotFound = () => {
   return (
     <div>
